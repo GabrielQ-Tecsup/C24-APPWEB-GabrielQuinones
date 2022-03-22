42f1edda6cda95c40c57e3895bc902acaa3c7a6a
GabrielQ-Tecsup <iron45104@gmail.com>
2022-03-22 18:13:24 -0500

    Lab1

---
+++ Semana1/Ejercicio1.js
@@ -0,0 +1,26 @@
+// Desarrolle una función que permita determinar cuál de tres
+// parámetros que se le proporcione a la función es mayor.
+
+// añadimos los valores
+let param1, param2, param3, i, mayor;
+
+// Agregamos los campos de lectura para los datos
+param1 = +prompt("Ingrese el primer numero")
+param2 = +prompt("Ingrese el segundo numero")
+param3 = +prompt("Ingrese el tercer numero")
+
+// creamos la funcion 
+if (param1 > param2) {
+    i = param1;
+}else{
+    i = param2;
+};
+if (i > param3) {
+    mayor = i
+}else{
+    mayor = param3
+};
+
+// Lo imprimimos en la consola
+console.log("El Numero Mayor es" , mayor);
+    
---
+++ Semana1/Ejercicio2.js
@@ -0,0 +1,25 @@
+// Mostrar los 50 primeros números múltiplos de 7,
+//  pero NO múltiplos de 2,3 o 5
+
+// Creamos una funcion para calcular si el numero es multiplo
+function multiple(valor, multiple) {
+        resultado = valor % multiple;
+        if(resultado==0)
+            return true;
+        else
+            return false;
+};
+
+// Primero creamos un array donde los valores se puedan almacenar
+let multiplo_7=[];
+
+// Creamos un bucle del 1 al 343 (ya que 7 x 49 es igual a 343)
+// y como el problema nos dice "los 50 primeros numeros" 7 x 0 tambien cuenta
+for(let i=0; i<=343; i++) {
+    // indicamos el numero dentro del if, en este caso es 7
+    if(multiple(i,7))
+        multiplo_7.push(i);
+};
+
+ // Imprimimos en la consola
+console.log("Los 50 primeros numeros multiplos de 7 son: ",multiplo_7);
