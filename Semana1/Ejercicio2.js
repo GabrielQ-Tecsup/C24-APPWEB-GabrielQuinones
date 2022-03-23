fec553fe7449e4b4c33f2a72bd61d6bb0ac1c943
GabrielQ-Tecsup <iron45104@gmail.com>
2022-03-22 21:13:13 -0500

    confirmar en "master"

--- Semana1/Ejercicio2.js
+++ Semana1/Ejercicio2.js
@@ -1,25 +1,15 @@
 // Mostrar los 50 primeros números múltiplos de 7,
 //  pero NO múltiplos de 2,3 o 5
 
-// Creamos una funcion para calcular si el numero es multiplo
-function multiple(valor, multiple) {
-        resultado = valor % multiple;
-        if(resultado==0)
-            return true;
-        else
-            return false;
+// Creamos la funcionalidad
+let multiplo_7 = [];
+let numero = 1330; 
+let dato;
+
+for (dato = 1; dato <= numero; dato++) {
+  if(dato%7 === 0 && dato%2 !== 0 && dato%3 !== 0 && dato%5 !== 0 ){
+    multiplo_7.push(dato);
+  }
 };
-
-// Primero creamos un array donde los valores se puedan almacenar.
-let multiplo_7=[];
-
-// Creamos un bucle del 1 al 343 (ya que 7 x 49 es igual a 343)
-// y como el problema nos dice "los 50 primeros numeros" 7 x 0 tambien cuenta
-for(let i=0; i<=343; i++) {
-    // indicamos el numero dentro del if, en este caso es 7
-    if(multiple(i,7))
-        multiplo_7.push(i);
-};
-
- // Imprimimos en la consola
-console.log("Los 50 primeros numeros multiplos de 7 son: ",multiplo_7);
+// Imprimimos en la consola
+console.log(multiplo_7);
